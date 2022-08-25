abacd54aaf3700e9141a013e9d731380f9b18148
MHabilArifin <mhabilarifin1414@gmail.com>
2022-08-25 22:49:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+document.getElementById("passInp1").addEventListener("input", function () {
+  document.getElementById("passInp2").value = document.getElementById("passInp1").value;
+});
